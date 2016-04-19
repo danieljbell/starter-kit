320dafd4eae09d3409b3c2e1b4298046cd8d4b81
danieljbell <djbell70@gmail.com>
2016-04-19 12:16:16 -0400

    Replaced 404

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <section>
 	<div class="site-width">
-		<h1>404, man</h1>
+		<h1>404</h1>
 		<form role="search" method="get" action="<?php echo site_url(); ?>">
             <input type="search" placeholder="Search TinderBox" value="" name="s" title="Search for:">
         </form>
